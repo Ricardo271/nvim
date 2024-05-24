338aca349913acd465a2aa151d9751efa97d8b91
Ricardo271 <ric.zanluchi@gmail.com>
2024-05-24 18:10:51 -0300

    Autocompletions

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,55 @@
 return {
-    {
-        'williamboman/mason.nvim',
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            local masonlspconfig = require("mason-lspconfig")
-            masonlspconfig.setup({
-                ensure_installed = {"lua_ls", "gopls", "clangd"} -- stylelint, prettierd, html-lsp
-            })
-            masonlspconfig.setup_handlers {
-                function(server_name)
-                    require("lspconfig")[server_name].setup({})
-                end,
-                lua_ls = function()
-                    require('lspconfig').lua_ls.setup({
-                        settings = {
-                            Lua = {
-                                runtime = {
-                                    version = 'LuaJIT'
-                                },
-                                diagnostics = {
-                                    globals = {'vim'},
-                                },
-                                workspace = {
-                                    library = {
-                                        vim.env.VIMRUNTIME,
-                                    }
-                                }
-                            }
-                        }
-                    })
-                end
-            }
-        end
-    },
-    {
-        "neovim/nvim-lspconfig",
-        config = function()
-            vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-            vim.keymap.set({'n', 'v'}, '<leader>ca', vim.lsp.buf.code_action, {})
-        end
-    },
+	{
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			local masonlspconfig = require("mason-lspconfig")
+			masonlspconfig.setup({
+				ensure_installed = { "lua_ls", "gopls", "clangd" }, -- stylelint, prettierd, html-lsp
+			})
+
+			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+			masonlspconfig.setup_handlers({
+				function(server_name)
+					require("lspconfig")[server_name].setup({
+						capabilities = lsp_capabilities,
+					})
+				end,
+				lua_ls = function()
+					require("lspconfig").lua_ls.setup({
+						capabilities = lsp_capabilities,
+						settings = {
+							Lua = {
+								runtime = {
+									version = "LuaJIT",
+								},
+								diagnostics = {
+									globals = { "vim" },
+								},
+								workspace = {
+									library = {
+										vim.env.VIMRUNTIME,
+									},
+								},
+							},
+						},
+					})
+				end,
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,9 +4,10 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = {"lua", "go", "c"},
+            ensure_installed = { "lua", "go", "c" },
+            auto_install = true,
             highlight = { enable = true },
-            indent = {enable = true },
+            indent = { enable = true },
         })
-    end
+    end,
 }

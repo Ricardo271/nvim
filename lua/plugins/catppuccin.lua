34e32e15bf5ca170fda4c78a1e1d7eb86c2ddd7d
Ricardo271 <ric.zanluchi@gmail.com>
2024-05-24 18:30:18 -0300

    Fixed transparency

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -5,5 +5,10 @@ return {
     priority = 1000,
     config = function()
         vim.cmd.colorscheme("catppuccin")
+
+        -- Makes so that the theme doesn't alter the background
+        -- Transparency is set in the terminal configs
+        vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     end
 }

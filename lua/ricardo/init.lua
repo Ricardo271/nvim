0046abdfa5c332e0f1660157fcc80e25db3c2332
Ricardo271 <ric.zanluchi@gmail.com>
2024-06-15 15:43:44 -0300

    Added highlight on yank

--- lua/ricardo/init.lua
+++ lua/ricardo/init.lua
@@ -1,5 +1,19 @@
 require("ricardo.set")
 require("ricardo.remap")
-
-
 -- The requires need to be in this specifc order so that the leader remps work
+
+
+local autocmd = vim.api.nvim_create_autocmd
+local augroup = vim.api.nvim_create_augroup
+local yank_group = augroup('HighlightYank', {})
+
+autocmd('TextYankPost', {
+    group = yank_group,
+    pattern = '*',
+    callback = function()
+        vim.highlight.on_yank({
+            higroup = 'IncSearch',
+            timeout = 40,
+        })
+    end,
+})

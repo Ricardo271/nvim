c369a69bfb393685cd57e882438e344b2b3d7bae
Ricardo271 <ric.zanluchi@gmail.com>
2025-05-16 13:53:17 -0300

    Formatting

--- lua/plugins/mini-files.lua
+++ lua/plugins/mini-files.lua
@@ -1,8 +1,12 @@
 return {
-	"echasnovski/mini.files",
-	version = false,
-    config = function ()
-        require("mini.files").setup()
+    "echasnovski/mini.files",
+    version = false,
+    config = function()
+        require("mini.files").setup({
+            options = {
+                permanent_delete = false,
+            },
+        })
         vim.keymap.set("n", "-", ":lua MiniFiles.open()<CR>", {})
-    end
+    end,
 }
